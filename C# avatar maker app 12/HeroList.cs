57f76f5e06d25ea4491226218adaa71882079994
Kulpot <kulpotwahu@gmail.com>
2024-02-20 18:44:10 +0800

    created HeroList.cs

---
+++ C# avatar maker app 12/HeroList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__avatar_maker_app_12
+{
+    public class HeroList
+    {
+    }
+}
